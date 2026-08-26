5877ff1337a36d9f7610f5fc20bb8735b756b7a1
bilal-rauf-dev <bilal.rauf282004@gmail.com>
2026-08-26 09:58:54 +0500

    fix: restore notification inbox rendering and add favicon

--- frontend/src/components/NotificationInbox.jsx
+++ frontend/src/components/NotificationInbox.jsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useMemo, useState } from 'react';
+import React, { Fragment, useEffect, useMemo, useState } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 import { BellIcon, CheckIcon, XMarkIcon } from '@heroicons/react/24/outline';
 import api from '../api/axios';
